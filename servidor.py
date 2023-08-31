b07d35bf94cf0f072fba311e8d6e55437af0c1e5
A01751277 <95493462+A01751277@users.noreply.github.com>
2023-08-31 17:32:44 -0600

    Actualización 4 - cambio a servidor.py

--- servidor.py
+++ servidor.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import numpy as np
+from joblib import load
+import os
 
+# Cargar el modelo
+dt = load("dt1.joblib")
+
+# Servidor (backend)
 # Generar el servidor (Back-end)
 servidorWeb = Flask(__name__)
 
 @servidorWeb.route("/holamundo", methods=['GET'])
+# def formulario() # No es necesario que corresponda el nombre de la función con la ruta
 def holamundo():
     return render_template('pagina1.html')
+
+# Envío de datos a través de JSON
+@servidorWeb.route("/modelo", methods=['POST'])
+def modeloPrediccion():
+    # Procesar los datos de entrada
+    contenido = request.json
+    print(contenido)
+    return jsonify({"resultado": "Hola"})
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
